223ebf7bf31d9262a8e3399282cf4063d3c94928
chemist-god <godtenkorang72@gmail.com>
2025-04-10 17:18:14 +0000

    update MongoDB connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI as string);
+        await mongoose.connect(process.env.MONGODB_URI as string);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
